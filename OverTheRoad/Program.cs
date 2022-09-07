a5d0a241c5073e7fc424e73e850da9dffdd866d0
fasdser <107477645+fasdser@users.noreply.github.com>
2022-09-07 18:58:49 +0300

    Add project files.

---
+++ OverTheRoad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverTheRoad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OverTheRoad(3,3));
+            Console.ReadLine();
+        }
+
+        public static long OverTheRoad(long address, long n)
+        {
+
+
+            return (address + (n - address) + (n + 1 - address)); 
+        }
+
+        public static long overTheRoad(long a, long n) => 2 * n - a + 1;
+    }
+}
